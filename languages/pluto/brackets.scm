68c5f4b1e66099c01b125cfdae5a2fd5fe46a715
Subrat <Subrat8225@gmail.com>
2026-04-26 11:52:10 +0530

    Update brackets.scm with PLUTO keyword bracket matching for PROCEDURE, IF, STRUCTS, OPERATOR

--- languages/pluto/brackets.scm
+++ languages/pluto/brackets.scm
@@ -1,3 +1,16 @@
-("{" @open "}" @close)
+; Standard brackets
 ("(" @open ")" @close)
 ("[" @open "]" @close)
+
+; PLUTO keyword brackets
+; PROCEDURE...END
+("PROCEDURE" @open "END" @close)
+
+; IF...END
+("IF" @open "END" @close)
+
+; STRUCTS...END
+("STRUCTS" @open "END" @close)
+
+; OPERATOR...END
+("OPERATOR" @open "END" @close)
